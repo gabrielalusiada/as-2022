98bd2ec61fce2a6e5013648819383257c742becf
gabrielalusiada <73133535+gabrielalusiada@users.noreply.github.com>
2022-03-07 15:48:30 +0000

    Create Animal.py
    add class Animal

---
+++ Animal.py
@@ -0,0 +1,40 @@
+class Animal:
+    def __init__(self):
+        self.name = ''
+        self.idade = 0
+        self.cor = ''
+
+    def comer():
+
+    def locomover():
+
+    def falar():
+
+    def dormir():
+        
+
+
+class Cao(Animal):
+    def __init__(self):
+        self.raca = ''
+        self.pelo = ''
+        self.porte = ''
+
+    def ladrar():
+
+
+
+class Gato(Animal):
+
+    def miar():
+
+    def saltar():
+
+
+
+class Peixe(Animal):
+
+    def nadar():
+
+
+    
